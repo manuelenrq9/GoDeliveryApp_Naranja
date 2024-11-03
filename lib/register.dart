86710e8769830a8c503a94c9801b1b627d09bd60
aledance <aferreira.17@est.ucab.edu.ve>
2024-11-03 03:32:54 -0400

    feat: :art: formulario register
    coloque los formularios y algunas funcionalidades de esos formularios  en el register.dart

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Quitar la etiqueta de desarrollador
+      home: const RegisterScreen(),
+    );
+  }
+}
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
@@ -8,21 +24,206 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  bool _isObscure = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'images/LogoLetrasGoDely.png',
-              width: 270,
-              height: 270,
-            ),
-          ],
+      body: Center(
+        child: SingleChildScrollView(
+          padding:
+              const EdgeInsets.only(top: 50.0), // Add padding to move inputs up
+          child: Column(
+            children: [
+              Image.asset(
+                'images/LogoLetrasGoDely.png',
+                width: 270,
+                height: 270,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Ingresa Tus Datos',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child:
+                    //Campo Correo/Usuario
+                    TextField(
+                        decoration: InputDecoration(
+                  hintText: 'Nombre de Usuario',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.person),
+                )),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child:
+                    //Campo Correo/Usuario
+                    TextField(
+                        decoration: InputDecoration(
+                  hintText: 'Correo Electronico',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.email),
+                )),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextField(
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    hintText: 'Contraseña',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextField(
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    hintText: 'Confirmar Contraseña',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child:
+                    //Campo Correo/Usuario
+                    TextField(
+                        decoration: InputDecoration(
+                  hintText: 'Región',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.travel_explore),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child:
+                    //Campo Correo/Usuario
+                    TextField(
+                        decoration: InputDecoration(
+                  hintText: '+58| Telefono',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.phone),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child:
+                    //Campo Correo/Usuario
+                    TextField(
+                        decoration: InputDecoration(
+                  hintText: 'Fecha de Nacimiento',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.calendar_month),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2100),
+                        builder: (BuildContext context, Widget? child) {
+                          return Theme(
+                            data: ThemeData.light().copyWith(
+                              colorScheme: const ColorScheme.light(
+                                primary: Color(0xFFFF7000), // Color naranja
+                                onPrimary: Color.fromARGB(255, 15, 15,
+                                    15), // Color del texto en el botón
+                                surface: Color.fromARGB(255, 255, 255,
+                                    255), // Color de fondo del calendario
+                                onSurface: Colors
+                                    .black, // Color del texto en el calendario
+                              ),
+                              dialogBackgroundColor: Colors.white,
+                            ),
+                            child: child!,
+                          );
+                        },
+                      );
+                      if (pickedDate != null) {
+                        // Handle the selected date
+                      }
+                    },
+                  ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(
+                        0xFFFF7000), // Color de fondo del botón (#FF7000)
+                    minimumSize:
+                        const Size(double.infinity, 50), // Tamaño del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10), // Bordes redondeados
+                    ),
+                  ),
+                  onPressed: () {
+                    // Acción del botón
+                  },
+                  child: const Text(
+                    'Crear Cuenta',
+                    style: TextStyle(
+                      color: Colors.white, // Color del texto blanco
+                      fontWeight: FontWeight.bold, // Letras en negrita
+                      fontSize: 16, // Tamaño del texto
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
