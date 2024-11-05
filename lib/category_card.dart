cf0f9a0a32a7723b5a839bda1757e606edf687fe
manuelenrq9 <manuel.enrqt@gmail.com>
2024-11-04 21:08:26 -0400

    feat: :sparkles: Pantalla de menu principal creada
    Se creo el archivo main_menu.dart, donde se mostrará el menú principal de la aplicación, y asociado a el se crean los widgets necesarios para esta interfáz, tales como product_card, combo_card y category_card

---
+++ lib/category_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final String iconPath;
+    const CategoryCard({
+    super.key,
+    requited this.title,
+    r
+tiuq
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      color:const Color(0xFFFF7000),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(50) ),),
+      ),
+    );
+  }
+}
---
+++ lib/main_menu.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
+
+  @override, 
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar( // Color de la barra superior
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            // Accion para abrir el menu
+          },
+        ),
+        title: Center(
+          child: Image.asset(
+            '',
+            height: 40,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Accion para busqueda de productos
+            },
+          ),
+        ],
+      ),
+      body:  const Center(
+        child: SingleChildScrollView(
+            
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+
+              ],
+            ),                
+        )
+      )
+    );
+  }
+}
