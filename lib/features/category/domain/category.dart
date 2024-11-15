4cb592f92de5a98696f3e51535a5457b05add493
CristhianM <mendescristhian66@gmail.com>
2024-11-15 08:29:40 -0400

    feat: :sparkles: Implementacion de local storage a categoria e interfaz All Categories
    Se crea la interfaz All categories y se implementa el local storage a categoria e interfaz All Categories

--- lib/features/category/domain/category.dart
+++ lib/features/category/domain/category.dart
@@ -9,19 +9,21 @@ class Category {
     required this.image,
   });
 
+    // Método de fábrica para convertir JSON en un objeto Category
   factory Category.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'id': String id,
-        'name': String name,
-        'image': String image,
-      } =>
-        Category(
-          id: id,
-          name: name,
-          image: image,
-        ),
-      _ => throw const FormatException('Failed to load categoria'),
+    return Category(
+      id: json['id'],      // Accede al valor de 'id' en el mapa
+      name: json['name'],  // Accede al valor de 'name' en el mapa
+      image: json['image'], // Accede al valor de 'image' en el mapa
+    );
+  }
+
+  // Método toJson para convertir el objeto Category en un Map<String, dynamic>
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'image': image,
     };
   }
 }
