cad83edc7f12ab61b5dfb1e6ed5618af41494869
Cristhian Mendes <mendescristhian66@gmail.com>
2025-01-10 01:54:03 -0400

    merge orderHistoryDynamic/manuel to mergeRamas/Cris

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import location
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
